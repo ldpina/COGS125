4cb2b87900cf2cd69f6e5e8d1a245c97dadf8005
ldpina <ldpina@ucsd.edu>
2024-10-29 14:13:03 -0700

    helo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <h1>Luis Pina</h1>
       <img src="https://dogtime.com/wp-content/uploads/sites/12/2023/11/GettyImages-1329412827-e1701097258260.jpg" />
       <p>This is my portfolio</p>
+      
     </div>
   );
 }
